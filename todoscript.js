902d0e78d512feeb97e4df25851fdbba6df0f3c2
Jonbailey1 <34288882+Jonbailey1@users.noreply.github.com>
2023-09-25 15:42:56 -0400

    Todo List Implementation

---
+++ todoscript.js
@@ -0,0 +1,31 @@
+// Adds user inputted item into to-do list
+// Creates functional complete and delete buttons
+function addItem() {
+    var taskText = document.getElementById("item").value;
+    if (taskText.trim() === "") {
+        return;
+    }
+
+    var itemList = document.getElementById("item-list");
+    var newItem = document.createElement("li");
+    newItem.innerHTML = `
+        <span>${taskText}</span>
+        <button onclick="completeItem(this)">Complete</button>
+        <button onclick="deleteItem(this)">Delete</button> 
+    `;
+
+    itemList.appendChild(newItem);
+    document.getElementById("item").value = "";
+}
+
+// Crosses out completed item
+function completeItem(button) {
+    var taskItem = button.parentElement;
+    taskItem.classList.toggle("completed");
+}
+
+// Deletes item and removes from list
+function deleteItem(button) {
+    var taskItem = button.parentElement;
+    taskItem.remove();
+}
