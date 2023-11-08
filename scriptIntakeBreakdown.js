d3e912034a80b19a68312bab2e41f5868870851c
Jessica Wu <emailme20022@yahoo.com>
2023-11-08 02:49:33 -0500

    pie chart changes based on index variables: needs renaming

--- scriptIntakeBreakdown.js
+++ scriptIntakeBreakdown.js
@@ -2,25 +2,15 @@ const greenPercentage = parseInt(localStorage.getItem('greenPercentage')) || 33;
 const yellowPercentage = parseInt(localStorage.getItem('yellowPercentage')) || 33;
 const redPercentage = parseInt(localStorage.getItem('redPercentage')) || 33;
 
-const pieChart = document.getElementById('pieChart');
-const percentageDisplay = document.getElementById('percentageDisplay');
+const greenPercentageDisplay = document.getElementById('greenPercentageDisplay');
+const yellowPercentageDisplay = document.getElementById('yellowPercentageDisplay');
+const redPercentageDisplay = document.getElementById('redPercentageDisplay');
 
-function updatePieChart() {
-    const totalPercentage = greenPercentage + yellowPercentage + redPercentage;
-    const greenPercentageValue = (greenPercentage / totalPercentage) * 100;
-    const yellowPercentageValue = (yellowPercentage / totalPercentage) * 100;
-    const redPercentageValue = (redPercentage / totalPercentage) * 100;
-
-    pieChart.style.background = `conic-gradient(
-      from 0deg,
-      palegreen ${greenPercentageValue}%,
-      lightgoldenrodyellow ${greenPercentageValue}% ${greenPercentageValue + yellowPercentageValue}%,
-      lightcoral ${greenPercentageValue + yellowPercentageValue}% ${greenPercentageValue + yellowPercentageValue + redPercentageValue}%
-    )`;
-
-    const selectedPercentage = greenPercentage;
-    percentageDisplay.textContent = `Selected Portion: ${selectedPercentage}%`;
+function updatePercentageDisplays() {
+    greenPercentageDisplay.textContent = `Green: ${greenPercentage}%`;
+    yellowPercentageDisplay.textContent = `Yellow: ${yellowPercentage}%`;
+    redPercentageDisplay.textContent = `Red: ${redPercentage}%`;
 }
 
-// Call updatePieChart() on page load
-updatePieChart();
+// Call updatePercentageDisplays() on page load
+updatePercentageDisplays();
