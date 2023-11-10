81e4253a46f36450130500aa722f13ffc8497517
Jessica Wu <emailme20022@yahoo.com>
2023-11-09 19:42:04 -0500

    calorie breakdown

--- scriptIntakeBreakdown.js
+++ scriptIntakeBreakdown.js
@@ -1,31 +1,32 @@
-const greenPercentage = parseInt(localStorage.getItem('greenPercentage')) || 33;
-const yellowPercentage = parseInt(localStorage.getItem('yellowPercentage')) || 33;
+const bluePercentage = parseInt(localStorage.getItem('bluePercentage')) || 33;
+const brownPercentage = parseInt(localStorage.getItem('brownPercentage')) || 33;
 const redPercentage = parseInt(localStorage.getItem('redPercentage')) || 33;
 
 const pieChart = document.getElementById('pieChart');
-const greenPercentageDisplay = document.getElementById('greenPercentageDisplay');
-const yellowPercentageDisplay = document.getElementById('yellowPercentageDisplay');
+const bluePercentageDisplay = document.getElementById('bluePercentageDisplay');
+const brownPercentageDisplay = document.getElementById('brownPercentageDisplay');
 const redPercentageDisplay = document.getElementById('redPercentageDisplay');
 
 function updatePieChart() {
-    const totalPercentage = greenPercentage + yellowPercentage + redPercentage;
+    const totalPercentage = bluePercentage + brownPercentage + redPercentage;
 
     // Calculate angles for each segment
-    const greenAngle = (greenPercentage / totalPercentage) * 360;
-    const yellowAngle = (yellowPercentage / totalPercentage) * 360;
+    const blueAngle = (bluePercentage / totalPercentage) * 360;
+    const brownAngle = (brownPercentage / totalPercentage) * 360;
     const redAngle = (redPercentage / totalPercentage) * 360;
 
     // Apply CSS styles to create pie chart segments
     pieChart.style.background = `conic-gradient(
       from 0deg,
-      limegreen 0deg ${greenAngle}deg,
-      yellow ${greenAngle}deg ${greenAngle + yellowAngle}deg,
-      tomato ${greenAngle + yellowAngle}deg ${greenAngle + yellowAngle + redAngle}deg
+      steelblue 0deg ${blueAngle}deg,
+      sienna ${blueAngle}deg ${blueAngle + brownAngle}deg,
+      darkorange ${blueAngle + brownAngle}deg ${blueAngle + brownAngle + redAngle}deg
     )`;
 
-    greenPercentageDisplay.textContent = `Carbohydrates: ${greenPercentage}%`;
-    yellowPercentageDisplay.textContent = `Protein: ${yellowPercentage}%`;
-    redPercentageDisplay.textContent = `Fat: ${redPercentage}%`;
+    bluePercentageDisplay.innerHTML = `<strong>${bluePercentage}%</strong>`;
+    brownPercentageDisplay.innerHTML = `<strong>${brownPercentage}%</strong>`;
+    redPercentageDisplay.innerHTML = `<strong>${redPercentage}%</strong>`;
+    
 }
 
 // Call updatePieChart() on page load
