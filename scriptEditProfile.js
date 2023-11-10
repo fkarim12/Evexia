248703f34e187a51df1c72e112a575e74454d424
Jessica Wu <emailme20022@yahoo.com>
2023-11-09 22:08:34 -0500

    profile restrictions

--- scriptEditProfile.js
+++ scriptEditProfile.js
@@ -7,7 +7,17 @@ window.onload = function () {
     document.getElementById('weight').value = localStorage.getItem('weight') || '';
     document.getElementById('goalWeight').value = localStorage.getItem('goalWeight') || '';
     document.getElementById('goals').value = localStorage.getItem('goals') || '';
+
+    // Get the close button element
+    var closeButton = document.getElementById('closeButton');
+
+    // Attach a click event listener to the close button
+    closeButton.addEventListener('click', function() {
+        var popOutContainer = document.getElementById('popOutContainer');
+        popOutContainer.style.display = 'none'; // Hide the pop-up
+    });
 };
+
 
 function saveProfile() {
     var name = document.getElementById('name').value;
@@ -17,6 +27,47 @@ function saveProfile() {
     var weight = document.getElementById('weight').value;
     var goalWeight = document.getElementById('goalWeight').value;
     var goals = document.getElementById('goals').value;
+
+
+    // Check name format
+    if (!name.includes(' ')) {
+        showError(1);
+        return;
+    }
+
+    // Check email format
+    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+        showError(2);
+        return;
+    }
+
+    // Check age range
+    if (age < 0 || age > 110) {
+        showError(3);
+        return;
+    }
+
+    // Check height format (number' number")
+    var heightRegex = /^\d+'\s?\d+"$/;
+    if (!heightRegex.test(height)) {
+        showError(4);
+        return;
+    }
+
+    // Check weight format (only numbers)
+    var weightRegex = /^\d+$/;
+    if (!weightRegex.test(weight)) {
+        showError(5);
+        return;
+    }
+
+    // Check goal weight format (only numbers)
+    var goalWeightRegex = /^\d+$/;
+    if (!goalWeightRegex.test(goalWeight)) {
+       showError(5);
+       return;
+    }
 
     // Store the data in localStorage
     localStorage.setItem('name', name);
@@ -29,6 +80,36 @@ function saveProfile() {
 
     // Redirect to the user profile page
     window.location.href = 'userProfile.html';
+}
+
+function showError(errorCode) {
+    var errorMessage = '';
+    switch (errorCode) {
+        case 1:
+            errorMessage = 'Please enter your first and last name.';
+            break;
+        case 2:
+            errorMessage = 'Please enter a valid email.';
+            break;
+        case 3:
+            errorMessage = 'Please enter a valid age between 0 and 110.';
+            break;
+        case 4:
+            errorMessage = 'Please enter height in the format (feet\' inches").';
+            break;
+        case 5:
+            errorMessage = 'Please enter weight using only numbers.';
+            break;
+        default:
+            errorMessage = 'An error occurred.';
+            break;
+    }
+
+    var popOutContent = document.querySelector('.pop-out-content');
+    popOutContent.textContent = errorMessage;
+
+    var popOutContainer = document.getElementById('popOutContainer');
+    popOutContainer.style.display = 'block';
 }
 
 function showKeyboard(event, imageId) {
@@ -61,3 +142,4 @@ document.body.addEventListener('click', function (event) {
         h3Element.classList.remove('scrollable-h3');
     }
 });
+
