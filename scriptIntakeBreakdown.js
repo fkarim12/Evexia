c120e03c76bec0ab90b06fc6c4b113498fd9bb72
Jessica Wu <emailme20022@yahoo.com>
2023-11-10 08:06:24 -0500

    colorblind changes

--- scriptIntakeBreakdown.js
+++ scriptIntakeBreakdown.js
@@ -1,3 +1,5 @@
+const sections = document.querySelectorAll('.section');
+
 const bluePercentage = parseInt(localStorage.getItem('bluePercentage')) || 33;
 const brownPercentage = parseInt(localStorage.getItem('brownPercentage')) || 33;
 const redPercentage = parseInt(localStorage.getItem('redPercentage')) || 33;
@@ -7,30 +9,54 @@ const bluePercentageDisplay = document.getElementById('bluePercentageDisplay');
 const brownPercentageDisplay = document.getElementById('brownPercentageDisplay');
 const redPercentageDisplay = document.getElementById('redPercentageDisplay');
 
-function updatePieChart() {
-    const totalPercentage = bluePercentage + brownPercentage + redPercentage;
+const colorblindButton = document.getElementById("colorblind");
+let colorblindMode = false;
 
-    // Calculate angles for each segment
-    const blueAngle = (bluePercentage / totalPercentage) * 360;
-    const brownAngle = (brownPercentage / totalPercentage) * 360;
-    const redAngle = (redPercentage / totalPercentage) * 360;
+colorblindButton.addEventListener("click", function () {
+  colorblindMode = !colorblindMode;
+  updateColors();
+});
 
-    // Apply CSS styles to create pie chart segments
-    pieChart.style.background = `conic-gradient(
-      from 0deg,
-      steelblue 0deg ${blueAngle}deg,
-      sienna ${blueAngle}deg ${blueAngle + brownAngle}deg,
-      darkorange ${blueAngle + brownAngle}deg ${blueAngle + brownAngle + redAngle}deg
-    )`;
+function updateColors() {
+  const steelblueColor = colorblindMode ? "lightblue" : "steelblue";
+  const siennaColor = colorblindMode ? "palegoldenrod" : "sienna";
+  const darkorangeColor = colorblindMode ? "lightsalmon" : "darkorange";
 
-    bluePercentageDisplay.innerHTML = `<strong>${bluePercentage}%</strong>`;
-    brownPercentageDisplay.innerHTML = `<strong>${brownPercentage}%</strong>`;
-    redPercentageDisplay.innerHTML = `<strong>${redPercentage}%</strong>`;
-    
+  sections[0].style.backgroundColor = steelblueColor;
+  sections[1].style.backgroundColor = siennaColor;
+  sections[2].style.backgroundColor = darkorangeColor;
+
+  updatePieChart();
 }
 
-// Call updatePieChart() on page load
+// Call updateColors on page load
+updateColors();
+
+
+function updatePieChart() {
+  const totalPercentage = bluePercentage + brownPercentage + redPercentage;
+
+  const blueAngle = (bluePercentage / totalPercentage) * 360;
+  const brownAngle = (brownPercentage / totalPercentage) * 360;
+  const redAngle = (redPercentage / totalPercentage) * 360;
+
+  // Set pie chart colors dynamically
+  pieChart.style.background = `conic-gradient(
+    from 0deg,
+    ${getComputedStyle(sections[0]).backgroundColor} 0deg ${blueAngle}deg,
+    ${getComputedStyle(sections[1]).backgroundColor} ${blueAngle}deg ${blueAngle + brownAngle}deg,
+    ${getComputedStyle(sections[2]).backgroundColor} ${blueAngle + brownAngle}deg ${blueAngle + brownAngle + redAngle}deg
+  )`;
+
+  bluePercentageDisplay.innerHTML = `<strong>${bluePercentage}%</strong>`;
+  brownPercentageDisplay.innerHTML = `<strong>${brownPercentage}%</strong>`;
+  redPercentageDisplay.innerHTML = `<strong>${redPercentage}%</strong>`;
+}
+
 updatePieChart();
+
+// Rest of your existing code...
+
 
 const learnMoreButton = document.getElementById("learnMoreButton");
 const popOutContainer = document.getElementById("popOutContainer");
