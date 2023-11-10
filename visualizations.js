7f6fae1c6499f4c754defe77cc22287c2036c940
Jessica Wu <emailme20022@yahoo.com>
2023-11-10 15:34:41 -0500

    fixed on screen keyboard for visualizations

--- visualizations.js
+++ visualizations.js
@@ -29,3 +29,29 @@ new Chart(calorieCtx, {
     lineTension: 0,
   },
 });
+
+
+function showKeyboard(event, imageId) {
+  event.stopPropagation(); // Stop the event from propagating to document.body
+  var keyboardImage = document.getElementById(imageId);
+  keyboardImage.style.display = 'block';
+
+  var weightForm = document.getElementById('weightForm');
+  weightForm.classList.add('scrollable'); // Add the scrollable class to the form
+
+  // Calculate the distance to scroll based on the target pixel value
+  var scrollDistance = Math.max(0, keyboardImage.getBoundingClientRect().top - 640);
+
+  // Scroll the page up until the bottom of the page reaches pixel 640
+  window.scrollBy(0, -scrollDistance);
+}
+
+// Hide the keyboard image and remove the class when the screen is clicked elsewhere
+document.body.addEventListener('click', function (event) {
+  if (event.target.id !== 'keyboardImage') {
+    document.getElementById('keyboardImage').style.display = 'none';
+
+    var weightForm = document.getElementById('weightForm');
+    weightForm.classList.remove('scrollable'); // Remove the scrollable class from the form
+  }
+});
